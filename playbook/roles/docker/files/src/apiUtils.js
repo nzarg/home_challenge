578f64c24f24ad224bc6bf866940d8da8a152eef
nzarg <lucasboglione@gmail.com>
2023-06-27 14:36:00 +1200

    working on the Express.js Form

---
+++ playbook/roles/docker/files/src/apiUtils.js
@@ -0,0 +1,124 @@
+const axios = require('axios');
+const { URLSearchParams } = require('url');
+
+// Function to retrieve the client ID
+function getClientId(apiKey) {
+  return axios.get(`https://api.sitehost.nz/1.2/api/get_info.json?apikey=${apiKey}`)
+    .then(response => {
+      const { status, return: data } = response.data;
+      if (status && data && data.client_id) {
+        return data.client_id;
+      } else {
+        throw new Error('Failed to retrieve client ID');
+      }
+    })
+    .catch(error => {
+      throw new Error('Failed to retrieve client ID');
+    });
+}
+
+// Function to add a Domain 
+function addDomain(apiKey, clientId, domain) {
+  const data = new URLSearchParams();
+  data.append('apikey', apiKey);
+  data.append('client_id', clientId);
+  data.append('domain', domain);
+
+  const options = {
+    url: 'https://api.sitehost.nz/1.2/dns/create_domain.json',
+    method: 'POST',
+    data: data.toString(),
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+  };
+
+  return axios(options)
+    .then(response => response.data)
+    .catch(error => {
+      throw new Error('Failed to add Domain');
+    });
+}
+
+// Function to add a DNS record
+function addDNSRecord(apiKey, clientId, domain, type, name, content, prio = '0') {
+  const data = new URLSearchParams();
+  data.append('apikey', apiKey);
+  data.append('client_id', clientId);
+  data.append('domain', domain);
+  data.append('type', type);
+  data.append('name', `${name}.${domain}`);
+  data.append('content', content);
+  data.append('prio', prio);
+
+  const options = {
+    url: 'https://api.sitehost.nz/1.2/dns/add_record.json',
+    method: 'POST',
+    data: data.toString(),
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+  };
+
+  return axios(options)
+    .then(response => response.data)
+    .catch(error => {
+      throw new Error('Failed to add DNS record');
+    });
+}
+
+
+// Function to list DNS records
+function getDnsList(apiKey, clientId, domain) {
+  const url = `https://api.sitehost.nz/1.2/dns/list_records.json?apikey=${apiKey}&client_id=${clientId}&domain=${domain}`;
+
+  const options = {
+    method: 'GET',
+    url,
+  };
+
+  return axios(options)
+    .then(response => {
+      const { status, return: responseData } = response.data;
+      if (status && responseData) {
+        return responseData;
+      } else {
+        throw new Error('Failed to retrieve DNS records');
+      }
+    })
+    .catch(error => {
+      throw new Error('Failed to retrieve DNS records');
+    });
+}
+
+
+// Function to list Domains
+function getDomainsList(apiKey, clientId) {
+  const url = `https://api.sitehost.nz/1.2/dns/list_domains.json?apikey=${apiKey}&client_id=${clientId}&filters[domain]=.com`;
+
+  const options = {
+    method: 'GET',
+    url,
+  };
+
+  return axios(options)
+    .then(response => {
+      const { status, return: responseData } = response.data;
+      if (status && responseData) {
+        return responseData.data;
+      } else {
+        throw new Error('Failed to retrieve List of Domains');
+      }
+    })
+    .catch(error => {
+      throw new Error('Failed to retrieve List of Domains');
+    });
+}
+
+module.exports = {
+  getClientId,
+  addDomain,
+  addDNSRecord,
+  getDnsList,
+  getDomainsList,
+};
--- playbook/roles/docker/files/src/server.js
+++ playbook/roles/docker/files/src/server.js
@@ -1,135 +1,18 @@
 const express = require('express');
-const { URLSearchParams } = require('url');
 const app = express();
-const axios = require('axios');
+const { getClientId, addDomain, addDNSRecord, getDnsList, getDomainsList } = require('./apiUtils');
 
+const apiKey = 'f5567a857d478d9cc8bd96e0115f8c2d07e9c68ff4fb1cd4a4f072b0629cd679';
 
 // Middleware to parse form data
 app.use(express.urlencoded({ extended: false }));
-
-const API_KEY = 'f5567a857d478d9cc8bd96e0115f8c2d07e9c68ff4fb1cd4a4f072b0629cd679';
-
-// Function to retrieve the client ID
-function getClientId(apiKey) {
-  return axios.get(`https://api.sitehost.nz/1.2/api/get_info.json?apikey=${apiKey}`)
-    .then(response => {
-      const { status, return: data } = response.data;
-      if (status && data && data.client_id) {
-        return data.client_id;
-      } else {
-        throw new Error('Failed to retrieve client ID');
-      }
-    })
-    .catch(error => {
-      throw new Error('Failed to retrieve client ID');
-    });
-}
-
-// Function to add a Domain 
-function addDomain(clientId, domain) {
-  const data = new URLSearchParams();
-  data.append('apikey', API_KEY);
-  data.append('client_id', clientId);
-  data.append('domain', domain);
-
-  const options = {
-    url: 'https://api.sitehost.nz/1.2/dns/create_domain.json',
-    method: 'POST',
-    data: data.toString(),
-    headers: {
-      'Content-Type': 'application/x-www-form-urlencoded',
-    },
-  };
-
-  return axios(options)
-    .then(response => response.data)
-    .catch(error => {
-      throw new Error('Failed to add Domain');
-    });
-}
-
-// Function to add a DNS record
-function addDNSRecord(clientId, domain, type, name, content, prio = '0') {
-  const data = new URLSearchParams();
-  data.append('apikey', API_KEY);
-  data.append('client_id', clientId);
-  data.append('domain', domain);
-  data.append('type', type);
-  data.append('name', `${name}.${domain}`);
-  data.append('content', content);
-  data.append('prio', prio);
-
-  const options = {
-    url: 'https://api.sitehost.nz/1.2/dns/add_record.json',
-    method: 'POST',
-    data: data.toString(),
-    headers: {
-      'Content-Type': 'application/x-www-form-urlencoded',
-    },
-  };
-
-  return axios(options)
-    .then(response => response.data)
-    .catch(error => {
-      throw new Error('Failed to add DNS record');
-    });
-}
-
-
-// Function to list DNS records NOT WORKING
-function getDnsList(apiKey, clientId, domain) {
-  const url = `https://api.sitehost.nz/1.2/dns/list_records.json?apikey=${apiKey}&client_id=${clientId}&domain=${domain}`;
-
-  const options = {
-    method: 'GET',
-    url,
-  };
-
-  return axios(options)
-    .then(response => {
-      const { status, return: responseData } = response.data;
-      if (status && responseData) {
-        return responseData;
-      } else {
-        throw new Error('Failed to retrieve DNS records');
-      }
-    })
-    .catch(error => {
-      throw new Error('Failed to retrieve DNS records');
-    });
-}
-
-
-// Function to list Domains
-function getDomainsList(apiKey, clientId) {
-  const url = `https://api.sitehost.nz/1.2/dns/list_domains.json?apikey=${apiKey}&client_id=${clientId}&filters[domain]=.com`;
-
-  const options = {
-    method: 'GET',
-    url,
-  };
-
-  return axios(options)
-    .then(response => {
-      const { status, return: responseData } = response.data;
-      if (status && responseData) {
-        return responseData.data;
-      } else {
-        throw new Error('Failed to retrieve List of Domains');
-      }
-    })
-    .catch(error => {
-      throw new Error('Failed to retrieve List of Domains');
-    });
-}
-
 
 // Route for displaying the form to add a DNS record
 app.get('/', async (req, res) => {
 
   try {
-    const clientId = await getClientId(API_KEY);
-    const domainsListResponse = await getDomainsList(API_KEY, clientId);
+    const clientId = await getClientId(apiKey);
+    const domainsListResponse = await getDomainsList(apiKey, clientId);
 
     // Generate the options base on a array
     function arrayToDropdwon (array){
@@ -203,8 +86,8 @@ app.post('/add-dns-zone', async (req, res) => {
   const domain = req.body.domain;
 
   try {
-    const clientId = await getClientId(API_KEY);
-    const addDomainResponse = await addDomain(clientId, domain)
+    const clientId = await getClientId(apiKey);
+    const addDomainResponse = await addDomain(apiKey, clientId, domain)
     res.send(`
 
       <h1>Dns Zone Added</h1>
@@ -213,7 +96,7 @@ app.post('/add-dns-zone', async (req, res) => {
           `<p>${addDomainResponse.msg}".</p>`
         )
       }
-
+      <a href="/">Next</a>
     `);
 
   } catch (error) {
@@ -233,9 +116,9 @@ app.post('/add-dns-record', async (req, res) => {
   const prio = req.body.prio;
 
   try {
-    const clientId = await getClientId(API_KEY);
-    const dnsRecord = await addDNSRecord(clientId, domain, type, name, content, prio);
-    const dnsList = await getDnsList(API_KEY, clientId, domain);
+    const clientId = await getClientId(apiKey);
+    const dnsRecord = await addDNSRecord(apiKey, clientId, domain, type, name, content, prio);
+    const dnsList = await getDnsList(apiKey, clientId, domain);
     res.send(`
 
       <h1>DNS Record Status</h1>
